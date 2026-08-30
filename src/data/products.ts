9d07828c20902fe6cbfc5bbc86b525d0d75b9b6f
anas.haimani <haimanianas005@gmail.com>
2026-08-30 23:51:34 +0100

    prodmod

--- src/data/products.ts
+++ src/data/products.ts
@@ -48,8 +48,8 @@ export const PRODUCTS: Product[] = [
     activationTemperature: null,
     stock: true,
     images: [
-      `${import.meta.env.BASE_URL}products/NoFire-device-studio.jpeg`,
-      `${import.meta.env.BASE_URL}products/NoFire-device-label.jpeg`,
+      `${import.meta.env.BASE_URL}products/BlockFire-device-studio.jpeg`,
+      `${import.meta.env.BASE_URL}products/BlockFire-device-label.jpeg`,
     ],
   },
 ];
