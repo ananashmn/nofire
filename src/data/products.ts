33169bf0e3cac5e109304c6550704d14dc109f7d
anas.haimani <haimanianas005@gmail.com>
2026-08-31 00:17:29 +0100

    yarebihhhhh

--- src/data/products.ts
+++ src/data/products.ts
@@ -48,8 +48,8 @@ export const PRODUCTS: Product[] = [
     activationTemperature: null,
     stock: true,
     images: [
-      `${import.meta.env.BASE_URL}products/NoFire-device-studio.jpeg`,
-      `${import.meta.env.BASE_URL}products/NoFire-device-label.jpeg`,
+      `${import.meta.env.BASE_URL}products/blockFire-device-studio.jpeg`,
+      `${import.meta.env.BASE_URL}products/blockFire-device-label.jpeg`,
     ],
   },
 ];
